eed1696ead93a7ef5745862db1541b6940142ee7
Mateo Gastaldi <mateogastaldi@gmail.com>
2025-07-03 19:21:18 -0300

    Tests

--- src/test/java/com/example/tp/LicenciaTest.java
+++ src/test/java/com/example/tp/LicenciaTest.java
@@ -17,18 +17,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class LicenciaTest {
-    
+
     @Autowired
     LicenciaService_impl licenciaService;
 
     LocalDate date1 = LocalDate.of(2000, 10, 10);
     LocalDate date2 = LocalDate.of( 2009, 1, 1);
-    TitularDTO tit1 = new TitularDTO("Mateo", "Gastaldi", "45489", "DNi", date1, "Llerena 2260", "AB", "+", true);
-    TitularDTO tit2 = new TitularDTO("Juan", "Juancin", "24291870", "DNi", date2, "Llerena 2260", "AB", "-", false);
+    TitularDTO tit1 = new TitularDTO("Mateo", "Gastaldi", "45489", "DNI", date1, "Llerena 2260", "AB", "+", true);
+    TitularDTO tit2 = new TitularDTO("Juan", "Juancin", "24291870", "DNI", date2, "Llerena 2260", "AB", "-", false);
+    //este tiene q aparecer en la base de datos
+    TitularDTO tit3 = new TitularDTO("Juan", "Gimenez", "15486318", "DNi", date2, "Llerena 2260", "AB", "-", false);
 
 
     LicenciaDTO licenciaDTO1 = new LicenciaDTO(tit1, "No tiene nada", "A");
-    LicenciaDTO licenciaDTO2 = new LicenciaDTO(tit1, "No tiene nada", "A");
+    LicenciaDTO licenciaDTO2 = new LicenciaDTO(tit3, "No tiene nada", "A");
     LicenciaDTO licenciaDTO3 = new LicenciaDTO(tit2, "Ciego de un ojo", "B");
 
 
@@ -36,39 +38,24 @@ public class LicenciaTest {
     void pruebaEdadMinimaValida() {
         Assertions.assertTrue(licenciaService.edadMinima(licenciaDTO1));
     }
+    @Test
     void pruebaEdadMinimaInvalida() {
-        Assertions.assertTrue(licenciaService.edadMinima(licenciaDTO1));
+        Assertions.assertThrows(LicenciaDatosInvalidosException.class,()->{licenciaService.edadMinima(licenciaDTO2);});
     }
     @Test
     void pruebaLicenciaProfesional() {
         Assertions.assertTrue(licenciaService.profesional(licenciaDTO1));
     }
     @Test
-    void pruebaLicenciaRepetida() {
-        Assertions.assertTrue(licenciaService.repetida(licenciaDTO1));
+    void pruebaLicenciaRepetidaInvalida() {
+        Assertions.assertThrows(LicenciaDatosInvalidosException.class,()->{licenciaService.repetida(licenciaDTO1);}); //EN la base de datos este usuario ya tiene una licencia de esta clase
+    }
+    @Test
+    void pruebaLicenciaRepetidaValida() {
+        Assertions.assertFalse(licenciaService.repetida(licenciaDTO2)); //EN la base de datos este ususario no tiene una licencia de esta clase
     }
     @Test
     void pruebaTitularInexistente() {
-        Assertions.assertThrows(LicenciaDatosInvalidosException.class,()->{licenciaService.edadMinima(licenciaDTO2);});
-    }
-    @Test
-    void pruebaTitularExistente() {
-        Assertions.assertFalse(licenciaService.profesional(licenciaDTO2));
-    }
-    @Test
-    void ncontextLoads() {
-        Assertions.assertTrue(licenciaService.repetida(licenciaDTO2));
-    }
-    @Test
-    void nombre() {
-        Assertions.assertFalse(licenciaService.edadMinima(licenciaDTO3));
-    }
-    @Test
-    void contextLreroads() {
-        Assertions.assertTrue(licenciaService.profesional(licenciaDTO3));
-    }
-    @Test
-    void contextLoadsere() {
-        Assertions.assertTrue(licenciaService.repetida(licenciaDTO3));
+        Assertions.assertThrows(LicenciaDatosInvalidosException.class,()->{licenciaService.edadMinima(licenciaDTO3);});
     }
 }
